5b82b60176afe1ab3cedec7567c30527d0f0e50b
dshuais <1137896420@qq.com>
2024-08-06 17:03:53 +0800

    ✨ feat: 新增自动导入组件

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
+import AutoImport from "unplugin-auto-import/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [uni()],
+  plugins: [
+    uni(),
+
+    AutoImport({
+      imports: ["vue", "uni-app"],
+      dts: "src/typings/auto-imports.d.ts",
+    }),
+  ],
 });
