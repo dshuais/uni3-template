82d0e3db905abe9070681c7c37faf59f6aa26e6f
dshuais <1137896420@qq.com>
2024-08-09 15:18:00 +0800

    ✨ feat: 新增pages配置

---
+++ src/pages/common/detail/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <view>
+    detail
+  </view>
+</template>
---
+++ src/pages/common/webview/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <view>
+    webview
+  </view>
+</template>
