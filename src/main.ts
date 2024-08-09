7ddda63d5a68c938b02a1f029e5a8452e6a0554f
dshuais <1137896420@qq.com>
2024-08-09 18:39:58 +0800

    ✨ feat: 新增pinia

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import { createSSRApp } from 'vue';
+import { createPinia } from 'pinia';
+
 import App from './App.vue';
+
 export function createApp() {
-  const app = createSSRApp(App);
+  const app = createSSRApp(App)
+    .use(createPinia());
+
   return {
     app
   };
